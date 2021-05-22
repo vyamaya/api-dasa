150274396f6912ba25b41e10406ebe9053cc7889
Vinicius <yamaya@ymail.com>
2021-05-22 17:17:27 -0300

    refactor: test laboratorios

--- src/test/labs.test.js
+++ src/test/labs.test.js
@@ -10,24 +10,16 @@ const request = (url, method, data) => {
     return axios({ url, method, data})
 }
 
-test('should get labs', async () => {
-    //given - dado que
-    const lab1 = await new Lab({
-        nome: generate(),
-        endereco: generate(),
-        status: "ativo"
-    })
+test ('should get labs', async () => {
+    const lab1 = await new Lab({nome: generate(), endereco: generate(),status: "ativo"})
     lab1.save(lab1)
-    //when - quando acontecer
     const response = await request('http://localhost:3000/laboratorios', 'get')
-
     const labs = response.data
-    //then - então
     expect(labs).toHaveLength(1)
     await Lab.deleteOne({ status: "ativo" })
 })
 
-test.only('should save labs', async () => {
+test ('should save labs', async () => {
     const data = { nome: generate(), endereco: generate(), status: "ativo"}
     const response = await request('http://localhost:3000/laboratorios', 'post', data)
     const lab = response.data
