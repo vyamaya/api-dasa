fcc158195cdd65ea9d044a6a52da0dd78d77c0be
vyamaya <vyamaya@hotmail.com>
2021-05-24 16:51:05 -0300

    refactor: connect mongodb

--- src/infra/mongodb.js
+++ src/infra/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/dasa')
+mongoose.connect('mongodb+srv://deploy:uploaddeploy@cluster0.divc9.mongodb.net/dasa')
 
 module.exports = mongoose
