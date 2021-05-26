cc2f79439fcf05e42ca192a9bba86e31106db9ba
Vinicius <yamaya@ymail.com>
2021-05-26 08:52:44 -0300

    feat: add rota getById exames

--- src/route/examRoute.js
+++ src/route/examRoute.js
@@ -3,6 +3,7 @@ const examController = require('../controller/examController')
 const examRouter = express.Router()
 
 examRouter.get('/exames', examController.getActive)
+examRouter.get('/exames:id', examController.getById)
 examRouter.post('/exames', examController.insert)
 examRouter.put('/exames', examController.update)
 examRouter.delete('/exames', examController.deleteById)
